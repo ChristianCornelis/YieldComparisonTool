97615dcee33d1ecdd425a896dcbcb868d3c79344
ChristianCornelis <ccorneli@uoguelph.ca>
2020-02-10 04:45:57 -0500

    Housekeeping for driver program

--- src/main/java/project/InputHandler.java
+++ src/main/java/project/InputHandler.java
@@ -1,4 +1,123 @@
 package project;
 
-public class InputHandler {
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Temporary class to handle input. This will be removed in the future, so it's kinda gross for the time being: )
+ */
+public class InputHandler extends PromptHelper {
+
+    private Scanner inputScanner;
+
+    /**
+     * Constructor.
+     */
+    public InputHandler() {
+        this.inputScanner = new Scanner(System.in);
+    }
+
+
+    /**
+     * Builds a string representing the status of the maps containing yields.
+     * @param producerYields producer yields map
+     * @param statsCanYields  statsCan yields map
+     * @return the pre-formatted string.
+     */
+    public String getYieldsStatus(Map producerYields, Map statsCanYields) {
+        String toReturn = "\nLoaded yields status:\n\t- ";
+        if (producerYields != null) {
+            toReturn += mapStatus("Producer yields", true) + "\n\t- ";
+        } else {
+            toReturn += mapStatus("Producer yields", false) + "\n\t- ";
+        }
+        if (statsCanYields != null) {
+            toReturn += mapStatus("Statistics Canada yields", true) + "\n";
+        } else {
+            toReturn += mapStatus("Statistics Canada yields", false) + "\n";
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * Checks if an action is valid.
+     * @param validActions array of valid actions
+     * @param action action to check
+     * @return true or false
+     */
+    private Boolean actionIsValid(int[] validActions, int action) {
+        for (int v : validActions) {
+            if (v == action) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Converts an action to an integer. Handles format exceptions.
+     * @param action the action to convert
+     * @return the action as an integer.
+     */
+    private int convertInputAction(String action) {
+        int inputAction = -1;
+        try {
+            inputAction = Integer.parseInt(action);
+        } catch (NumberFormatException e) {
+            inputAction = -1;
+        }
+        return inputAction;
+    }
+
+    /**
+     * Chooses an action from a prompt string.
+     * @param promptString The string to prompt.
+     * @param validInputs Valid input chouces.
+     * @return the chosen valid choice.
+     */
+    public int chooseAction(String promptString, ArrayList validInputs) {
+        String input = "";
+        int inputInt = -1;
+        System.out.println(promptString);
+        while (!validInputs.contains(inputInt)) {
+            input = inputScanner.next();
+            inputInt = convertInputAction(input);
+            if (!validInputs.contains(inputInt)) {
+                System.out.println(promptString);
+            }
+        }
+        return inputInt;
+    }
+
+    /**
+     * Chooses an action from a prompt string.
+     * @param promptString The string to prompt.
+     * @param validInputs Valid input chouces.
+     * @return the chosen valid choice.
+     */
+    public int chooseAction(String promptString, int[] validInputs) {
+        String input = "";
+        int inputInt = -1;
+        System.out.println(promptString);
+        while (!actionIsValid(validInputs, inputInt)) {
+            input = inputScanner.next();
+            inputInt = convertInputAction(input);
+
+            if (!actionIsValid(validInputs, inputInt)) {
+                System.out.println(promptString);
+            }
+        }
+        return inputInt;
+    }
+
+    /**
+     * Wrapper on scanner.next() for getting super basic input.
+     * @return the input.
+     */
+    public String getBasicInput() {
+        return inputScanner.next();
+    }
 }
+
--- src/main/java/project/PromptHelper.java
+++ src/main/java/project/PromptHelper.java
@@ -1,4 +1,76 @@
 package project;
 
-public class PromptHelper {
+public abstract class PromptHelper {
+
+    public static final int IMPORT_PRODUCER_CSV = 0;
+    public static final int IMPORT_STATSCAN_CSV = 1;
+    public static final int COMPARE_PRODUCER_STATSCAN_YIELDS = 2;
+
+    public static final int IMPORT_KG_PER_HA = 0;
+    public static final int IMPORT_LB_PER_AC = 1;
+    public static final int IMPORT_BU_PER_AC = 2;
+
+    public static final int CANCEL_TASK = 3;
+    public static final int QUIT = 4;
+
+    /**
+     * Builds a string representing the status of a given map.
+     * @param mapName the name of the map
+     * @param isPopulated whether the map is populated
+     * @return the formatted string.
+     */
+    public String mapStatus(String mapName, Boolean isPopulated) {
+        return "The " + mapName + " yield map " + (isPopulated ? ("is populated.") : ("is not populated."));
+    }
+
+    /**
+     * Helper to build a prompt for choosing a year.
+     * @return the string.
+     */
+    public String yearsPrompt() {
+        return "Enter a year to compare yields from:\n";
+    }
+
+    /**
+     * Helper to build prompt for units.
+     * @param customPrompt a string to be prepended to the prompt. For re-using this method.
+     * @return the pre-built prompt to be output.
+     */
+    public String unitsPrompt(String customPrompt) {
+        return  customPrompt +
+                "(" + Crop.KG_PER_HA + ") - Kilograms per hectare (kg/ha)" +
+                "\n(" + Crop.LBS_PER_AC + ") - Pounds per acre (lbs/ac)" +
+                "\n(" + Crop.BU_PER_AC + ") - Bushels per acre (bu/ac)" +
+                "\n(" + CANCEL_TASK + ") - Cancel this task";
+    }
+
+    /**
+     * Helper to represent prompt for entering a crop type to compare against.
+     * @return the prompt
+     */
+    public String cropPrompt() {
+        return "Enter the crop type you wish to compare yields for:\n";
+    }
+
+    /**
+     * Gets the prompt for a file location to import.
+     * @return the prompt
+     */
+    public String fileLocationPrompt() {
+        return "Enter the path to the CSV you would like to import.\n" +
+                "Paths should be relative to " + System.getProperty("user.dir") + "\n";
+    }
+
+    /**
+     * Prompt for an action to perform.
+     * @return the string to output.
+     */
+    public String actionPrompt() {
+        return "What would you like to do?\n" +
+                "\n(" + IMPORT_PRODUCER_CSV + ") - Import a producer yield CSV." +
+                "\n(" + IMPORT_STATSCAN_CSV + ") - Import a Statistics Canada average yield CSV." +
+                "\n(" + COMPARE_PRODUCER_STATSCAN_YIELDS +
+                ") - Compare producer yield data against Statistics Canada average yield data." +
+                "\n(" + QUIT + ") - Quit";
+    }
 }
