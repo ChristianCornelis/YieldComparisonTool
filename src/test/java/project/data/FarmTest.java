f68d32a5dc22d57433e8e426d5e54ab9f0da3a86
ChristianCornelis <ccorneli@uoguelph.ca>
2020-03-08 22:13:01 -0400

    Adding Farm class tests. Small change to toString needed

---
+++ src/test/java/project/data/FarmTest.java
@@ -0,0 +1,95 @@
+package project.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings("MissingJavadocMethod")
+public class FarmTest {
+    private Farm testFarm;
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Before
+    public void setup() {
+        testFarm = new Farm("Farm", "Guelph", "Corn", 100, Crop.KG_PER_HA, "Prod", 2020);
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testGetName() {
+        assertEquals(testFarm.getName(), "Farm");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testGetLocation() {
+        assertEquals(testFarm.getLocation(), "Guelph");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testGetProducer() {
+        assertEquals(testFarm.getProducer(), "Prod");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testSetLocation() {
+        testFarm.setLocation("Space");
+        assertEquals(testFarm.getLocation(), "Space");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testSetName() {
+        testFarm.setName("Sus");
+        assertEquals(testFarm.getName(), "Sus");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testSetProducer() {
+        testFarm.setProducer("Prod2");
+        assertEquals(testFarm.getProducer(), "Prod2");
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testToString() {
+        String exp = "Farm{name='Farm', location='Guelph', Crop{units=" +
+                Crop.KG_PER_HA + ", type='Corn', yield=100.0, year=2020}}";
+        assertEquals(testFarm.toString(), exp);
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    @Test
+    public void testToMap() {
+        Map<String, Object> exp = new HashMap<>();
+        exp.put("name", "Farm");
+        exp.put("location", "Guelph");
+        exp.put("producer", "Prod");
+        exp.put("yield", 100.0);
+        exp.put("type", "Corn");
+        exp.put("units", Crop.KG_PER_HA);
+        exp.put("year", 2020);
+        assertEquals(testFarm.toMap(), exp);
+    }
+
+    @SuppressWarnings("MissingJavadocMethod")
+    public static void main(String[] args) {
+
+        Result result = JUnitCore.runClasses(CropTest.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
