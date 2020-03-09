b666ba9818679e2dfb269d2dba24b2901f8da497
ChristianCornelis <ccorneli@uoguelph.ca>
2020-03-08 21:40:14 -0400

    Suppressing checkstyle warnings in tests

--- src/test/java/project/converters/ConverterTest.java
+++ src/test/java/project/converters/ConverterTest.java
@@ -8,37 +8,42 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import project.Exceptions;
 
+@SuppressWarnings("MissingJavadocMethod")
+/**
+ * Class to test the converter class used to convert between emtric and imperial units.
+ */
 public class ConverterTest {
 
     private Converter testConverter;
 
-
+    @SuppressWarnings("MissingJavadocMethod")
     @Before
     public void setup() {
         testConverter = new Converter();
     }
 
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testAcresToHectares() {
         double acres = 2.4710538147;
         double ha = testConverter.acresToHectares(acres);
         assertEquals(ha, 1, 0.001);  //allow precision to be slightly off
     }
-
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testHectaresToAcres() {
         double ha = 0.4046856422;
         double ac = testConverter.hectaresToAcres(ha);
         assertEquals(ac, 1, 0.001);  //allow precision to be slightly off
     }
-
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testLbsToKilos() {
         double lbs = 2.20462;
         double kgs = testConverter.lbsToKilos(lbs);
         assertEquals(kgs, 1, 0.001);
     }
-
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testKilosToLbs() {
         double kgs = 0.453592;
@@ -46,6 +51,7 @@ public class ConverterTest {
         assertEquals(lbs, 1, 0.001);
     }
 
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testKgPerHaToLbsPerAc() {
         double kgPerHa = 1.12085;
@@ -53,6 +59,7 @@ public class ConverterTest {
         assertEquals(lbsPerAc, 1, 0.001);
     }
 
+    @SuppressWarnings("MissingJavadocMethod")
     @Test
     public void testLbsPerAcToKgPerHa() {
         double lbsPerAc = 0.892179;
@@ -60,17 +67,20 @@ public class ConverterTest {
         assertEquals(kgPerHa, 1, 0.001);
     }
 
+    @SuppressWarnings({"MissingJavadocMethod", "MethodName"})
     @Test
     public void TestBuPerAcToLbsPerAc_ValidBushelConversionKey() throws Exceptions.BushelsConversionKeyNotFoundException {
         double lbsPerAc = testConverter.buPerAcToLbsPerAc(1, "Corn");
         assertEquals(lbsPerAc, 56, 0.001);
     }
 
+    @SuppressWarnings({"MissingJavadocMethod", "MethodName"})
     @Test(expected = Exceptions.BushelsConversionKeyNotFoundException.class)
-    public void TestBuPerAcToLbsPerAc_InValidBushelConversionKey() throws Exceptions.BushelsConversionKeyNotFoundException {
+    public void TestBuPerAcToLbsPerAc_InvalidBushelConversionKey() throws Exceptions.BushelsConversionKeyNotFoundException {
         double lbsPerAc = testConverter.buPerAcToLbsPerAc(1, "Fruit Salad");
     }
 
+    @SuppressWarnings("MissingJavadocMethod")
     public static void main(String[] args) {
 
         Result result = JUnitCore.runClasses(ConverterTest.class);
